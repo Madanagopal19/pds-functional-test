aa1fddc153957349deee1554d21f811eb7d16997
Madanagopal19 <marunachalam@purestorage.com>
2022-05-04 13:24:18 +0530

    Added Default storage template

--- pkg/common/control_plane.go
+++ pkg/common/control_plane.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"net/http"
-
 	pds "github.com/portworx/pds-api-go-client/pds/v1alpha1"
 	api "github.com/portworx/pds-functional-test/pkg/api"
 )
@@ -28,55 +26,23 @@ func (cp *ControlPlane) GetRegistrationToken(tenantId string) string {
 	return token.GetToken()
 }
 
-func (cp *ControlPlane) CreateStorageOptionTemplate(tenantId string, fg bool, fs string, name string, repl int32, secure bool) (*pds.ModelsStorageOptionsTemplate, error) {
+func (cp *ControlPlane) CreateDeafaultStorageTemplate(tenantId string, name string) (*pds.ModelsStorageOptionsTemplate, error) {
 	log.Info("Creating storage option template.")
 	st := cp.components.StorageSettingsTemplate
-	template, err := st.CreateTemplate(tenantId, fg, fs, name, repl, secure)
+	templates, _ := st.ListTemplates(tenantId)
+	for _, template := range templates {
+		if template.GetName() == name {
+			templateId := template.GetId()
+			log.Infof("Template Name %s,  Template ID %s", name, templateId)
+			return st.GetTemplate(templateId)
+		}
+	}
+	template, err := st.CreateTemplate(tenantId, false, "ext4", name, 2, false)
 	if err != nil {
 		log.Errorf("Storage template creation failed with error - %v", err)
+
 	}
 	return template, err
-
-}
-
-func (cp *ControlPlane) UpdateStorageOptionTemplate(templateId string, fg bool, fs string, name string, repl int32, secure bool) (*pds.ModelsStorageOptionsTemplate, error) {
-	log.Info("Updating storage option template.")
-	st := cp.components.StorageSettingsTemplate
-	template, err := st.UpdateTemplate(templateId, fg, fs, name, repl, secure)
-	if err != nil {
-		log.Errorf("Storage template updation failed with error - %v", err)
-	}
-	return template, err
-}
-
-func (cp *ControlPlane) GetStorageOptionTemplate(templateId string) (*pds.ModelsStorageOptionsTemplate, error) {
-	log.Info("Getting storage option template.")
-	st := cp.components.StorageSettingsTemplate
-	template, err := st.GetTemplate(templateId)
-	if err != nil {
-		log.Errorf("Storage template updation failed with error - %v", err)
-	}
-	return template, err
-}
-
-func (cp *ControlPlane) ListStorageOptionTemplates(tenantId string) ([]pds.ModelsStorageOptionsTemplate, error) {
-	log.Info("Getting storage option template.")
-	st := cp.components.StorageSettingsTemplate
-	templates, err := st.ListTemplates(tenantId)
-	if err != nil {
-		log.Errorf("List Storage template failed with error - %v", err)
-	}
-	return templates, err
-}
-
-func (cp *ControlPlane) DeleteStorageOptionTemplate(templateId string) (*http.Response, error) {
-	log.Info("Delete storage option template.")
-	st := cp.components.StorageSettingsTemplate
-	response, err := st.DeleteTemplate(templateId)
-	if err != nil {
-		log.Errorf("Delete Storage template failed with error - %v", err)
-	}
-	return response, err
 }
 
 func (cp *ControlPlane) CreateResourceSettingTemplate(tenantId string, name string, dataServiceName string) error {
